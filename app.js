1750c95e947db3ad403bd70ef267b56f30afaaa5
unknown <maxime.warnier@DUCK310.dpdsi.ad.meteo.fr>
2014-05-22 07:39:03 +0100

    loop optimisation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var sourceElement = document.getElementById("audioSrcElement") ;
+var sourceElement = document.getElementById("audioSrcElement");
 
 var context = new AudioContext();
 var analyser = context.createAnalyser();
@@ -6,52 +6,49 @@ var analyser = context.createAnalyser();
 analyser.fftSize = 32;
 console.log(analyser.frequencyBinCount); // fftSize/2 = 32 data points
 
-var frequencyBlockArray = [] ;
+var frequencyBlockArray = [];
 
 for (var i = 0; i < analyser.frequencyBinCount; i++) {
 
     var elm = document.createElement("div");
 
-    elm.className="frequencyBlock" ;
+    elm.className = "frequencyBlock";
     frequencyBlockArray.push(elm);
 
     document.body.appendChild(elm);
 }
 
 var source = context.createMediaElementSource(sourceElement);
-    source.connect(analyser);
-    analyser.connect(context.destination);
+source.connect(analyser);
+analyser.connect(context.destination);
 
-sourceElement.addEventListener("play",function(e){
+sourceElement.addEventListener("play", function(e) {
     analyseLoop();
 });
 
 
 
-function analyseLoop(){
-    if(!sourceElement.paused){
+function analyseLoop() {
+    if (!sourceElement.paused) {
         var frequencyData = new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(frequencyData);
 
-        frequencyBlockArray.forEach(function(elm,i){
-            var val = frequencyData[i] ;
+        for (var i = 0,t = frequencyBlockArray.length; i < t; i++) {
+            var val = frequencyData[i];
             var red = val;
             var green = 255 - val;
             var blue = val / 2;
 
-            elm.style.background = "rgb("+red+","+green+","+blue+")";
-        });
-        //console.log(frequencyData);
-        //setTimeout(analyseLoop,100);
-        window.requestAnimationFrame(analyseLoop);
-    }
+            frequencyBlockArray[i].style.background = "rgb(" + red + "," + green + "," + blue + ")";
+        }
+    //console.log(frequencyData);
+    //setTimeout(analyseLoop,100);
+    window.requestAnimationFrame(analyseLoop);
+}
 }
 
 
 
-sourceElement.addEventListener("pause",function(e){
+sourceElement.addEventListener("pause", function(e) {
     console.log("pause");
 });
-
-
-
